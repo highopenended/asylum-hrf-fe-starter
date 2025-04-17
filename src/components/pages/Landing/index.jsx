da33b4f135fff4d8e33b6381c7e912d53c60f96f
Matt Glenn <highopenended@gmail.com>
2025-04-17 18:49:04 -0400

    Started setting up Landing Page

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,10 +1,10 @@
-// import pieChart from '../../../assets/pie-chart.png';
-// import lineGraph from '../../../assets/line-graph.png';
-// import barGraph from '../../../assets/bar-graph.png';
-// import paperStack from '../../../assets/paper-stack.jpg';
+import pieChart from '../../../assets/pie-chart.png';
+import lineGraph from '../../../assets/line-graph.png';
+import barGraph from '../../../assets/bar-graph.png';
+import paperStack from '../../../assets/paper-stack.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
-import {decodeBase64} from '../../../utils/decodeBase64.js';
+import { decodeBase64 } from '../../../utils/decodeBase64.js';
 
 /**
  * TODO: Ticket 1:
@@ -31,7 +31,22 @@ export const LandingPage = () => {
   };
 
   return (
-    <div className='flex-c w-[100vw] secondary-c'>
+    <div>
+      <div className='flex justify-center gap-8 w-full'>
+        <div className='w-1/4 h-auto'>
+          <img src={barGraph} alt='bar-graph' className='w-full h-full' />
+          <p>Search Grant Rates By Office</p>
+        </div>
+        <div className='w-1/4 h-auto'>
+          <img src={pieChart} alt='pie-chart' className='w-full h-full' />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+        <div className='w-1/4 h-auto'>
+          <img src={lineGraph} alt='line-graph' className='w-full h-full' />
+          <p>Search Grant Rates Over Time</p>
+        </div>
+      </div>
+      {/* <img src={paperStack} alt='paper-stack' className='w-1/4 h-auto mx-auto mt-4' /> */}
       Landing Page
       <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
     </div>
