4c5bc87f1856dadb7fcb4f4107822a73d5786282
Matt Glenn <highopenended@gmail.com>
2025-04-17 19:26:25 -0400

    WIP

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -32,23 +32,49 @@ export const LandingPage = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-8 w-full'>
-        <div className='w-1/4 h-auto'>
-          <img src={barGraph} alt='bar-graph' className='w-full h-full' />
-          <p>Search Grant Rates By Office</p>
+      <div className='flex flex-col gap-16 w-full h-auto'>
+
+        {/* Top Section */}
+        <div className='primary-c'>
+          <h1 className='text-6xl text-white text-center mb-8'>Asylum Office Grant Rate Tracker</h1>
+          <p className='text-white text-center m-8'>
+            The Asylum Office Grant Rate Tracker provides asylum seekers, researchers, policymakers, and the public an interactive tool to explore USCIS data on
+            Asylum Office decisions
+          </p>
         </div>
-        <div className='w-1/4 h-auto'>
-          <img src={pieChart} alt='pie-chart' className='w-full h-full' />
-          <p>Search Grant Rates By Nationality</p>
+
+        {/* Graphs Section */}
+        <div className='flex justify-center gap-24 w-full px-16'>
+          <div className='w-1/4'>
+            <img src={barGraph} alt='bar-graph' className='w-full h-full' />
+            <p className='text-center text-lg pt-4'>Search Grant Rates By Office</p>
+          </div>
+          <div className='w-1/4'>
+            <img src={pieChart} alt='pie-chart' className='w-full h-full' />
+            <p className='text-center text-lg pt-4'>Search Grant Rates By Nationality</p>
+          </div>
+          <div className='w-1/4'>
+            <img src={lineGraph} alt='line-graph' className='w-full h-full' />
+            <p className='text-center text-lg pt-4'>Search Grant Rates Over Time</p>
+          </div>
         </div>
-        <div className='w-1/4 h-auto'>
-          <img src={lineGraph} alt='line-graph' className='w-full h-full' />
-          <p>Search Grant Rates Over Time</p>
+
+        {/* Paper stack and description section */}
+        <div className='flex justify-center items-center gap-32 p-16 m-16'>
+          <div className='w-1/3'>
+            <img src={paperStack} alt='paper-stack' className='w-full h-auto' />
+          </div>
+          <div className='w-1/2'>
+            <p className='leading-relaxed'>
+              Human Rights First has created a search tool to give you a user-friendly way to explore a data set of asylum decisions between FY 2016 and May
+              2021 by the USCIS Asylum Office, which we received through a Freedom of Information Act request. You can search for information on asylum grant
+              rates by year, nationality, and asylum office. Visualize the data with charts and graphs, and download the data set.
+            </p>
+          </div>
         </div>
+
+        {/* <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div> */}
       </div>
-      {/* <img src={paperStack} alt='paper-stack' className='w-1/4 h-auto mx-auto mt-4' /> */}
-      Landing Page
-      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
     </div>
   );
 };
